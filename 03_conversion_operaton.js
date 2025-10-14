c06b289f0a98ac1e135f36e6ea22500997856609
parth2teja23 <parth2teja@gmail.com>
2025-10-14 08:25:39 +0530

    conversion of datatype

---
+++ 03_conversion_operaton.js
@@ -0,0 +1,30 @@
+// let score = false
+
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score); // Converts to number datatype
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN (Not a number)
+// true -> 1 , false -> 0
+// null -> 0
+// undefined -> NaN
+
+// let isLoggedIn = " ";
+// let booleanIsLoggedIn = Boolean(isLoggedIn); // Converts to boolean datatype
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true , 0 -> false
+// "" -> false , "Parth" -> true
+// null -> false
+// undefined -> false
+// "0" -> true
+// " " -> true
+
+let score = "33.4"
+let stringNumber = String(score); // Converts to string datatype
+console.log(stringNumber);
+console.log(typeof stringNumber);
